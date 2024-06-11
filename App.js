591ed6349213df413397be43a1a60d731e6932fc
roberts6 <roberts_oscar@hotmail.com>
2024-06-11 20:23:20 -0300

    agrego pantalla de estadisticas

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Home from './client/views/Home.js';
 import Jugadores from './client/views/Jugadores.js';
 import DetalleJugador from './client/views/DetalleJugador.js';
 import CrearJugador from './client/views/CrearJugador.js';
+import Estadisticas from './client/views/Estadisticas.js';
 
 
 const Stack = createStackNavigator();
@@ -17,6 +18,7 @@ export default function App() {
         <Stack.Screen name="Agregar Jugador" component={CrearJugador} />
         <Stack.Screen name="Jugadores" component={Jugadores} />
         <Stack.Screen name="Información Jugador" component={DetalleJugador} />
+        <Stack.Screen name="Estadísticas" component={Estadisticas} />
       </Stack.Navigator>
     </NavigationContainer>
   );
