c7d8e7c74bfd8775d8bf852c64f7801694c2a3f2
roberts6 <roberts_oscar@hotmail.com>
2024-06-11 13:07:30 -0300

    se agrega carpeta client y se reordenan carpetas

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from './views/Home.js';
-import Jugadores from './views/Jugadores.js';
-import DetalleJugador from './views/DetalleJugador.js';
-import CrearJugador from './views/CrearJugador.js';
+import Home from './client/views/Home.js';
+import Jugadores from './client/views/Jugadores.js';
+import DetalleJugador from './client/views/DetalleJugador.js';
+import CrearJugador from './client/views/CrearJugador.js';
 
 
 const Stack = createStackNavigator();
